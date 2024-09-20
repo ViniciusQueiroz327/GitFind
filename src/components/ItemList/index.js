e2802382fc6bf7af07d811f2ab78b94e189e06ac
Vinicius Silva Queiroz <81514002+ViniciusQueiroz327@users.noreply.github.com>
2024-09-20 12:49:22 -0300

    adding fetching features

---
+++ src/components/ItemList/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles.css'
+function ItemList({title, description}) {
+  return (
+    <div className='itemList'>
+        <strong>{title}</strong>
+        <p>{description}</p>
+        <hr />
+    </div>
+  )
+}
+
+export default ItemList;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,35 @@
+import { useState } from 'react';
 import { Header } from '../../components/Header';
-import background from '../../assets/imgs/background.svg'
-import profilePicture from '../../assets/imgs/profilePicture.jpeg'
+import background from '../../assets/imgs/background.svg';
+import ItemList from '../../components/ItemList';
 
 import './styles.css'
 
 function App() {
+
+  const [user, setUser] = useState('');
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repos, setRepos] = useState(null);
+
+  const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${user}`);
+    const newUser = await userData.json();
+
+    console.log('yep')
+
+    if(newUser.name) {
+      const {avatar_url, name, bio, login} = newUser;
+      setCurrentUser({avatar_url, name, bio, login})
+
+      const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+      const newRepos = await reposData.json();
+
+      if(newRepos.length) {
+        setRepos(newRepos);
+      }
+    }
+  }
+
   return (
     <div className="App">
       <Header />
@@ -12,18 +37,33 @@ function App() {
         <img src={background} className="background" alt="backgrond app"/>
         <div className="info">
           <div className='inputs'>
-            <input name="usuario" placeholder="@username"/>
-            <button>Buscar</button>
+            <input name="usuario" value={user} onChange={event => setUser(event.target.value)} placeholder="@username"/>
+            <button onClick={handleGetData}>Buscar</button>
           </div>
-          <div className='profileInfos'>
-            <img src={profilePicture} className="profilePicture" alt="profile"/>
-            <div>
-              <h3>Vinicius Silva Queiroz</h3>
-              <span>@ViniciusQueiroz327</span>
-              <p>Descrição</p>
+
+          {currentUser?.name ? (
+            <>
+            <div className='profileInfos'>
+              <img src={currentUser.avatar_url} className="profilePicture" alt="profile"/>
+              <div>
+                <h3>{currentUser.name}</h3>
+                <span>@{currentUser.login}</span>
+                <p>{currentUser.bio}</p>
+              </div>
             </div>
+            <hr />
+            </>
+          ) : null}
+
+          {repos?.length ? (
+            <div>
+            <h4 className="repositorio">Repositórios</h4>
+            {repos.map(repo => (
+              <ItemList title={repo.name} description={repo.description}/>
+            ))}
           </div>
-          <hr />
+          ) : null}
+
         </div>
       </div>
     </div>
